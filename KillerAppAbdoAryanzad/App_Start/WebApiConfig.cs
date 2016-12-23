7e52b9c653020b8c166e35c63ff06e6d224323ed
Abdo Aryanzad <a.aryanzad@student.fontys.nl>
2016-12-23 17:42:30 +0100

    WepApi toegevoegd, code opgeschoond

---
+++ KillerAppAbdoAryanzad/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace KillerAppAbdoAryanzad.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute(
+                "Default",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional}
+            );
+        }
+    }
+}
---
+++ KillerAppAbdoAryanzad/Controllers/WebApiController.cs
@@ -0,0 +1,45 @@
+﻿using KillerAppAbdoAryanzad.Database;
+using KillerAppAbdoAryanzad.Database.Repositories;
+using KillerAppAbdoAryanzad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KillerAppAbdoAryanzad.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        // GET api/<controller>
+        public List<TypeDrugs> Get()
+        {
+            TypeDrugsRepo typedrugsrepo = new TypeDrugsRepo(new MSSQLTypeDrugsContext());
+            List<TypeDrugs> typedrugs = typedrugsrepo.GetAll();
+            return typedrugs;
+            
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
